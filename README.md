d2218d609e86cdaa73a702a549f2dc668e227ed3
Jiri Kolarik <jiri.kolarik@wereinhq.com>
2016-09-16 22:23:37 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Google Cloud Storage to App Engine Image
+
+This application allows you to convert image from GCS to GAE which enables automatic image resizing by parameters
---
+++ main.py
@@ -0,0 +1,20 @@
+from google.appengine.ext import blobstore
+from google.appengine.api import images
+
+import webapp2
+import json
+
+class Main(webapp2.RequestHandler):
+    def get(self):
+        self.redirect('/')
+
+class Image(webapp2.RequestHandler):
+    def get(self):
+        imageFile = self.request.get('file')
+        gskey = blobstore.create_gs_key('/gs/uletit/' + imageFile)
+        imageUrl = images.get_serving_url(gskey)
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.out.write(json.dumps({ 'url': imageUrl }))
+
+app = webapp2.WSGIApplication([('/', Main),
+                               ('/image', Image)], debug=True)
